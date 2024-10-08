f55867673083d695d9585b6c00cf77d4e4731a17
Filip Elznic <elznicfilip@gmail.com>
2024-10-08 14:07:49 +0200

    -...

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,8 +1,11 @@
 import "./App.css";
-import Header from "./components/Header.jsx";
+
+console.log("App component loaded");
+
+import Header from "./components/Header";
 import Navbar from "./components/Navbar.jsx";
 import Introduction from "./components/Introduction.jsx";
-import WorkExperience from "./components/WorkExperience.jsx";
+import WorkExperience from "./components/WorkExpirience.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Technologies from "./components/Technologies.jsx";
 import Projects from "./components/Projects.jsx";
