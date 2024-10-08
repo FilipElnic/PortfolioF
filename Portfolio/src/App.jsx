a2ddff7a37a382b451b50844a5a62870ab62818e
Filip Elznic <elznicfilip@gmail.com>
2024-10-08 10:31:14 +0200

    test

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Header from "./components/Header";
+import Header from "./components/Header.jsx";
 import Navbar from "./components/Navbar";
 import Introduction from "./components/Introduction";
 import WorkExperience from "./components/WorkExpirience";
