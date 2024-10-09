2b530df538a16ea3a87835c3e4b79d919a5b7ef7
Filip Elznic <elznicfilip@gmail.com>
2024-10-09 09:36:35 +0200

    Cards

--- Portfolio/src/components/Card.jsx
+++ Portfolio/src/components/Card.jsx
@@ -1,9 +1,29 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 
-function Card() {
+function Card(props) {
   return (
     <>
-      <div className="custom-card-size custom-card mt-6"></div>
+      <div className="custom-card-size custom-card mt-6">
+        <div className="img-container">
+          <img
+            src={props.img}
+            className="card-img-top rounded-5 p-1 mb-2"
+            alt={props.title}
+          />
+        </div>
+        <div className="card-body">
+          <h5 className="card-title p-2 mb-2 d-flex justify-content-center">
+            {props.title}
+          </h5>
+          <p className="card-text p-4">{props.text}</p>
+          <a
+            href="#"
+            className="btn btn-primary d-flex justify-content-center m-3 rounded-3"
+          >
+            Explore Project
+          </a>
+        </div>
+      </div>
     </>
   );
 }
--- Portfolio/src/components/Cards.jsx
+++ Portfolio/src/components/Cards.jsx
@@ -1,5 +1,9 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Card from "./Card";
+import img1 from "../assets/images/project1.png";
+import img2 from "../assets/images/project2.png";
+import img3 from "../assets/images/project3.png";
+import img4 from "../assets/images/project4.png";
 
 function Cards() {
   return (
@@ -7,16 +11,32 @@ function Cards() {
       <div className="container-fluid bg-dark-custom ">
         <div className="row">
           <div className="col-lg-3 col-md-6 col-sm-12 mb-4">
-            <Card />
+            <Card
+              img={img1}
+              title="Weather Application"
+              text="This project was for practising API. You enter a city name and you recieve current temperature, humidity and wind speed."
+            />
           </div>
           <div className="col-lg-3 col-md-6 col-sm-12 mb-4">
-            <Card />
+            <Card
+              img={img2}
+              title="Infinity animation"
+              text="This project was for practising css animations. It is infinity image slider, you can see other example on this page aswell. "
+            />
           </div>
           <div className="col-lg-3 col-md-6 col-sm-12 mb-4">
-            <Card />
+            <Card
+              img={img3}
+              title="Can animation only CSS"
+              text="This project was for practising css animation. It is about hovering over a can and it will change it's design."
+            />
           </div>
           <div className="col-lg-3 col-md-6 col-sm-12 mb-4">
-            <Card />
+            <Card
+              img={img4}
+              title="NaucSeTelesa"
+              text="This project is my school year project. It is for student who want to learn mathematical bodies. It will be in czech language."
+            />
           </div>
         </div>
       </div>
